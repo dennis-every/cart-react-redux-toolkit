1166362678e6141062d0da67b87b267b6698476b
Dennis Every <dennis@ansyra.com>
2023-02-28 16:30:57 -0400

    Add Navbar with useSelector

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
+    <main>
+      <Navbar />
       <h1>Hello World!</h1>
-    </>
+    </main>
   );
 }
 
