20d1f2f37aa0f211d42764d27d6a91d8eeae41cf
Dennis Every <dennis@ansyra.com>
2023-02-28 18:57:25 -0400

    Add modalSlice

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import CartContainer from './components/CartContainer';
 import { useDispatch, useSelector } from 'react-redux';
 import { calculateTotals } from './features/cart/cartSlice';
 import { useEffect } from 'react';
+import Modal from './components/Modal';
 
 function App() {
   const { cartItems } = useSelector((state) => state.cart);
+  const { isOpen } = useSelector((state) => state.modal);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
