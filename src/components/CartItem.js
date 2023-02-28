13c95889dc49ed3019760c342ec6b66b703e148f
Dennis Every <dennis@ansyra.com>
2023-02-28 18:30:22 -0400

    Add increase, decrease cartItems

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,5 +1,5 @@
 import { ChevronDown, ChevronUp } from '../icons';
-import { removeItem } from '../features/cart/cartSlice';
+import { removeItem, increase, decrease } from '../features/cart/cartSlice';
 import { useDispatch } from 'react-redux';
 
 const CartItem = (props) => {
@@ -21,11 +21,24 @@ const CartItem = (props) => {
         </button>
       </div>
       <div>
-        <button className='amount-btn'>
+        <button
+          className='amount-btn'
+          onClick={() => dispatch(increase({ id }))}
+        >
           <ChevronUp />
         </button>
         <p className='amount'>{amount}</p>
-        <button className='amount-btn'>
+        <button
+          className='amount-btn'
+          onClick={() => {
+            if (amount === 1) {
+              dispatch(removeItem(id));
+              return;
+            } else {
+              dispatch(decrease({ id }));
+            }
+          }}
+        >
           <ChevronDown />
         </button>
       </div>
