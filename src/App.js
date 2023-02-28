4fe5b8a48ee9c7e4fed36c22a19b74138b08300b
Dennis Every <dennis@ansyra.com>
2023-02-28 17:05:23 -0400

    Add CartContainer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
 
 function App() {
   return (
     <main>
       <Navbar />
-      <h1>Hello World!</h1>
+      <CartContainer />
     </main>
   );
 }
